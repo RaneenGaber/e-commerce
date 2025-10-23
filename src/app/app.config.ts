fb299c881cb1aba17c8727264bcea53e08eac765
Raneen <you@example.com>
2025-10-24 02:03:07 +0300

    create auth interceptor to send token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,13 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {AuthInterceptor} from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([AuthInterceptor]))
   ]
 };
---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,18 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpEvent, HttpHandlerFn, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AuthService} from '../services/auth/auth';
+
+export function AuthInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+ let authService = inject(AuthService)
+  const token: string | null= authService.getToken();
+
+  if (token) {
+    const newReq  = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`),
+    });
+    return next(newReq);
+  }
+
+  return next(req);
+}
