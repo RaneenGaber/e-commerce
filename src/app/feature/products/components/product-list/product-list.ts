639cdbf9eb5e4b26a4c7c1faecc746db27082942
Raneen <you@example.com>
2025-10-24 11:12:04 +0300

    handle go to deatils

--- src/app/feature/products/components/product-list/product-list.ts
+++ src/app/feature/products/components/product-list/product-list.ts
@@ -1,6 +1,7 @@
 import {Component, input} from '@angular/core';
 import {Product} from '../../models/interfaces/product';
 import {ProductCard} from '../product-card/product-card';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -9,4 +10,11 @@ import {ProductCard} from '../product-card/product-card';
 })
 export class ProductList {
   products = input<Product[]>([]);
+
+  constructor(
+    public router: Router,
+  ) {}
+  navigateToDetails(productId: string): void {
+    this.router.navigate(['/products', productId]);
+  }
 }
