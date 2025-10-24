0a276c29eb58d50362f2b8f2651ebc275f60b7c5
Raneen <you@example.com>
2025-10-24 10:23:14 +0300

    create page header

---
+++ src/app/shared/components/header/header.ts
@@ -0,0 +1,28 @@
+import {Component, input} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgClass} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [NgClass,MatIconModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  pageTitle= input.required<string>();
+  breadcrumb = input<string[]>([]);
+
+  constructor(private router: Router) {}
+
+  goToPage(crumb: string ,last: boolean) {
+
+    if (!last){
+      this.router.navigate([`/${crumb.toLowerCase()}`]);
+    }
+  }
+
+  navigateToCart() {
+
+  }
+}
